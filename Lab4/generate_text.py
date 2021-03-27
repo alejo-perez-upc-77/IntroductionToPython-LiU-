1124bc40f5c0f440f29b4b31392b79a5c60bc011
Martynas Lukosevicius <martynas2514@gmail.com>
2021-03-27 15:48:03 +0100

    Revert "modified for testing"
    This reverts commit f1a9f6a8536f286d53590e4aa9649bdebc90da49.

--- Lab4/generate_text.py
+++ Lab4/generate_text.py
@@ -10,6 +10,8 @@ import text_stats as ts
 from random import choices
 import time
 
+
+
 def write_to_file(sentence, file_name):
     # Writing to file 
     with open(file_name, "w") as file1: 
@@ -26,11 +28,11 @@ def Convert(tup):
 
 
 if __name__ == "__main__":
-    start = time.time()
     try:
         file_name = sys.argv[1]
         starting_word = sys.argv[2]
         max_words = int(sys.argv[3])
+        start = time.time()
     except:
         print("Please provide all arguments correctly")
     else:
@@ -46,22 +48,29 @@ if __name__ == "__main__":
             Stop = True
             i = 0
             new_word = starting_word
+            word_dictionary = {}
             while (i<max_words) & Stop:
                 if new_word in ordered_words:
-                    following_words, weights  =  Convert(ts.get_most_common_following_words(new_word,word_flw))
-                    if len(following_words) != 0:
-                        new_word = choices(following_words,weights,k=1)[0]
+                    if new_word in word_dictionary:
+                        new_word = choices(word_dictionary[new_word][0],word_dictionary[new_word][1],k=1)[0]
                         word_sequence.append(new_word)
                     else:
-                        Stop = False
+                        following_words, weights  =  Convert(ts.get_most_common_following_words(new_word,word_flw))
+                        word_dictionary[new_word] = (following_words, weights)
+
+                        if len(following_words) != 0:
+                            new_word = choices(following_words, weights, k=1)[0]
+                            word_sequence.append(new_word)
+                        else:
+                            Stop = False
                 else:
                     print(f"word {new_word} does not exist!")
                     Stop = False
                 i+=1
-            write_to_file(" ".join(word_sequence), "_".join(("new",file_name)))
+            write_to_file(" ".join(word_sequence), "_".join(("new",file_name.split(".")[0])))
             end = time.time()
-            print((end-start)/max_words)
-
+            print((end - start)/max_words)
+            
 
 
         
